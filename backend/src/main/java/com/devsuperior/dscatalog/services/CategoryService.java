bd9f8c96dc0da391959105c30a050a6046070218
williamcamasil <williamcamargosilva@gmail.com>
2021-09-17 07:54:19 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -17,6 +18,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	/*Com a annotation Transactional, o próprio framework faz a transação entre o banco de dados
+	 * o readOnly melhora a performance do BD*/
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+/*Através desse arquivo, nós iremos inserir os comandos SQL para executar no banco h2, quando abri-lo será mostrado os dados já cadastrados*/
+
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Electronics');
+INSERT INTO tb_category (name) VALUES ('Computes');
